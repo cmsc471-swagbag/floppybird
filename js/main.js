f323b1abd0c06a7a675fa12ae4a1f3d0f4e648e9
Dayyan1212 <khan.dayyan@gmail.com>
2015-11-25 17:42:41 -0500

    Make stuff all random
    makes the pipes random

--- js/main.js
+++ js/main.js
@@ -496,7 +496,7 @@ function updatePipes()
    //add a new pipe (top height + bottom height  + pipeheight == 420) and put it in our tracker
    var padding = 80;
    var constraint = 420 - pipeheight - (padding * 2); //double padding (for top and bottom)
-   var topheight = Math.floor(pipesInit * constraint + padding); //add lower padding (Math.random()*constraint) +
+   var topheight = Math.floor(Math.random() * constraint + padding); //add lower padding (Math.random()*constraint) +
    var bottomheight = (420 - pipeheight) - topheight;
    var newpipe = $('<div class="pipe animated"><div class="pipe_upper" style="height: ' + topheight + 'px;"></div><div class="pipe_lower" style="height: ' + bottomheight + 'px;"></div></div>');
    $("#flyarea").append(newpipe);
