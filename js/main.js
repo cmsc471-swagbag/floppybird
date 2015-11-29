6e672abcd8070ee71ecc28c04e7d1a7b0bd6c468
cferrier1995 <ferriercory@gmail.com>
2015-11-29 15:34:33 -0500

    Reduced Tickrate and Removed Memory
    Tickrate is now at 60 again so game is potato friendly. If you want to
    access the 96 version for faster learning use:
    http://userpages.umbc.edu/~ferrier1/floppyFast/

--- js/main.js
+++ js/main.js
@@ -149,7 +149,7 @@ function startGame()
    pipesCounter = 0;
 
    //start up our loops
-   var updaterate = 1000.0 / 96.0 ; //60 times a second
+   var updaterate = 1000.0 / 60.0 ; //60 times a second
    loopGameloop = setInterval(gameloop, updaterate);
    loopPipeloop = setInterval(updatePipes, 1400);
    
